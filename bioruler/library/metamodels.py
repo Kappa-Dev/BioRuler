b4ef234d4336e81d5d446ac94c1d69f6e0ada334
Eugenia <yarutoua@gmail.com>
2016-07-12 11:42:52 +0200

    Metamodel removed

--- bioruler/library/metamodels.py
+++ bioruler/library/metamodels.py
@@ -55,9 +55,17 @@ metamodel_AG.add_nodes_from(
 
 metamodel_AG.add_edges_from(
     [
+        ("protein", "complex"),
+        ("residue", "complex"),
+        ("small_molecule", "complex"),
+        ("family", "complex"),
+        ("complex", "complex"),
+
         ("region", "protein"),
         ("residue", "protein"),
+
         ("residue", "region"),
+
         ("flag", "protein"),
         ("flag", "region"),
         ("flag", "residue"),
@@ -97,79 +105,5 @@ metamodel_AG.add_edges_from(
         ("family", "FAM_s"),
         ("complex", "FAM_s"),
         ("FAM_t", "family"),
-
-        ("COM_s", "COM"),
-        ("COM_t", "COM"),
-        ("protein", "COM_s"),
-        ("small_molecule", "COM_s"),
-        ("family", "COM_s"),
-        ("complex", "COM_s"),
-        ("COM_t", "complex")
-    ]
-)
-
-simple_metamodel_AG = TypedDiGraph(metametamodel_AG)
-
-simple_metamodel_AG.add_nodes_from(
-    [
-        ("protein", "agent"),
-        ("region", "agent"),
-        ("residue", "agent"),
-        ("family", "agent"),
-        ("complex", "agent"),
-        ("small_molecule", "agent"),
-        ("flag", "attribute_node"),
-        ("FAM", "action"),
-        ("COM", "action"),
-        ("BND", "action"),
-        ("BRK", "action"),
-        ("MOD", "action"),
-    ]
-)
-
-simple_metamodel_AG.add_edges_from(
-    [
-        ("region", "protein"),
-        ("residue", "protein"),
-        ("residue", "region"),
-        ("flag", "protein"),
-        ("flag", "region"),
-        ("flag", "residue"),
-        ("flag", "family"),
-        ("flag", "complex"),
-        ("flag", "small_molecule"),
-
-        ("protein", "BND"),
-        ("region", "BND"),
-        ("small_molecule", "BND"),
-        ("family", "BND"),
-        ("complex", "BND"),
-
-        ("BRK", "BND"),
-        ("BRK", "protein"),
-        ("BRK", "region"),
-        ("BRK", "family"),
-        ("BRK", "complex"),
-        ("BRK", "small_molecule"),
-
-        ("protein", "MOD"),
-        ("region", "MOD"),
-        ("family", "MOD"),
-        ("complex", "MOD"),
-        ("small_molecule", "MOD"),
-        ("MOD", "flag"),
-
-        ("protein", "FAM"),
-        ("region", "FAM"),
-        ("small_molecule", "FAM"),
-        ("family", "FAM"),
-        ("complex", "FAM"),
-        ("FAM", "family"),
-
-        ("protein", "COM"),
-        ("small_molecule", "COM"),
-        ("family", "COM"),
-        ("complex", "COM"),
-        ("COM", "complex")
     ]
 )
